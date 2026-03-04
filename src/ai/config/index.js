a4fae44437aa6f9e246a3674d79248be482fb8d1
Udit Kumar Tiwari <159471583+Udit004@users.noreply.github.com>
2026-03-04 17:59:11 +0530

    update the ai folder structure and perfomance also.

--- src/ai/config/index.js
+++
@@ -1,3 +0,0 @@
-export { createLLM, LLM_CONFIG } from './llmconfig';
-export { AI_CONSTANTS, EMBEDDING_CONFIG, PINECONE_CONFIG } from './constants';
-export { getLangSmithClient, isLangSmithEnabled, reinitializeLangSmith } from './langsmith';
